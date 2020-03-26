c17f1ca499cbba59e7af49f4d7dd075c00793429
KirigaiKabuto <tleugazy98@gmail.com>
2020-03-26 21:35:40 +0600

    sdfdfsdfds

---
+++ hw2/exampe2.py
@@ -0,0 +1,26 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a>b:
+    print("a>b")
+elif a>c:
+    print("a>c")
+if b>a:
+    print("b>a")
+if b>c:
+    print("b>c")
+#ex1
++
+3
+4
+Answer:7
+#ex2
+-
+3
+4
+Answer:-1
+!= проверка на не равно
+== проверка на равно
+
+
+
---
+++ hw2/example3.py
@@ -0,0 +1,9 @@
+znak = input()
+number1 = int(input())
+number2 = int(input())
+if znak=="+":
+    print(number1+number2)
+elif znak=="-":
+    print(number1-number2)
+else:
+    print("error")
