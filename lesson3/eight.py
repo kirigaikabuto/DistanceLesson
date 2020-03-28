a0083d29aea511f7e9cf48e2e431f52e522a185d
Your Name <tleugazy98.com>
2020-03-28 20:15:24 +0600

    asdsdads

---
+++ lesson3/eight.py
@@ -0,0 +1,20 @@
+# s = "hello"
+#h e l l o
+#0 1 2 3 4
+# n = len(s)
+# print(n)
+# print(s[0])
+# print(s[4])
+# print(s[5]) - >Error
+# for i in s:
+#     print(i)
+s = "Yerassyl"
+n = len(s)
+for i in range(n):
+    print(s[i])
+#s[n-1]-> последний элемент
+#s[0] ->первый элемент
+s = "abcdefghl"
+#Input:abcdefghl
+#012345678
+#Output:acegl
