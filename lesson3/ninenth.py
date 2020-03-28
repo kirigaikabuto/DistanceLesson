f8b31cee6f2cae2ffecba482aa784ef65e795720
Your Name <tleugazy98.com>
2020-03-28 20:31:41 +0600

    asdsdads

---
+++ lesson3/ninenth.py
@@ -0,0 +1,11 @@
+s = input()
+# for i in s:
+#     print(i)
+words=s.split(" ")
+n = len(words)
+print(n)
+for i in words:
+    if i=="hello":
+        print("Good Bue")
+    else:
+        print(i)
