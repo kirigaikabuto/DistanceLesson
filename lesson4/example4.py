012cdde2599acdd0980d21bc052705569205dd94
Your Name <tleugazy98.com>
2020-03-31 20:16:12 +0600

    asdsdads

--- lesson4/example4.py
+++ lesson4/example4.py
@@ -1,8 +1,12 @@
 arr=["Hello","1231321","!!@#@!","1231"]
 numbers=[]
 words=[]
-# n = "asdasdsa"
-# try:
-#     print(int(n))
-# except:
-#     print("Value Erorr")    
+n = len(arr)
+for i in range(n):
+    try:
+        n = int(arr[i])
+        numbers.append(arr[i])
+    except:
+        words.append(arr[i])
+print(numbers)
+print(words)
