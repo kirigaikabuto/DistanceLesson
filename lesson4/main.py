aa4e90c8fb6719e99f6492b4aca28cde15bfe677
Your Name <tleugazy98.com>
2020-03-31 19:15:58 +0600

    asdsdads

--- lesson4/main.py
+++ lesson4/main.py
@@ -1,5 +1,8 @@
-s = "Hello my dear Friend.Hi my name is Yerassyl.Are you?"
+s = "Hello my dear Friend.Hi my name is Yerassyl.Are you"
 key="my"
 key_count=0
-#code
-print(key_count)#2
+words = s.split(" ")
+for word in words:
+    if word == key:
+        key_count = key_count + 1
+print(key_count)
