a3eb3c4045e2332d8e605ffa6979087db2a53396
Your Name <tleugazy98.com>
2020-03-31 20:07:32 +0600

    asdsdads

--- lesson4/example4.py
+++ lesson4/example4.py
@@ -1,5 +1,8 @@
-n = "asdasdsa"
-try:
-    print(int(n))
-except:
-    print("Value Erorr")    
+arr=["Hello",1231321,"!!@#@!",1231]
+numbers=[]
+words=[]
+# n = "asdasdsa"
+# try:
+#     print(int(n))
+# except:
+#     print("Value Erorr")    
