73a2adfba0b13bb48eba43f2e67c74c61b359af1
Your Name <tleugazy98.com>
2020-04-04 19:35:02 +0600

    asdsdads

--- lesson5/main.py
+++ lesson5/main.py
@@ -1,7 +1,12 @@
-n = int(input())
-arr=[int(i) for i in input().split()]
-sumi=0
-for i in range(n):
-    sumi = sumi + arr[i]
-print(sumi)
-    
+arr=[42,123,3894,212,422]
+arr1=[]
+arr2=[]
+for i in range(0,3):
+    arr1.append(arr[i])
+for i in range(3,len(arr)):
+    arr2.append(arr[i])
+print(arr)
+print(arr1)
+print(arr2)
+arr3=arr1+arr2
+print(arr3)
