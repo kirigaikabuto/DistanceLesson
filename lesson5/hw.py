02ac73f836a5491f1974bef09de1a89df99e5223
Your Name <tleugazy98.com>
2020-04-04 20:59:26 +0600

    asdsdads

--- lesson5/hw.py
+++ lesson5/hw.py
@@ -1,11 +1,14 @@
-arr=[1,2,3,4,5,0]
+import math
+arr=[1,2,3,1,2,0]
 num = 3
 diff=[]
+#math
 for i in range(len(arr)):
     diff.append(num-arr[i])
+print(diff)
 mini=diff[0]
 minindex=0
-for i in range(diff):
+for i in range(len(diff)):
     if diff[i]<mini:
         mini=diff[i]
         minindex=i
