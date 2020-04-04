a1a97d4005a9b683008116746ffca42ec8c143b4
Your Name <tleugazy98.com>
2020-04-04 19:42:19 +0600

    asdsdads

---
+++ lesson5/second.py
@@ -0,0 +1,10 @@
+arr=[1,2,3,12312313,131,3]
+#1 -> 0         arr[0] -> 1
+#2 -> 1         arr[1] -> 2
+#12312313 -> 3  arr[3] -> 12312313
+n = len(arr)
+print(n)
+for i in range(6):
+    print(i)
+    # if arr[i]%2==0:
+    #     print(arr[i])
