442e910e33893bd14bfb24b3fdfde3550208715c
Your Name <tleugazy98.com>
2020-04-07 19:34:14 +0600

    asdsdads

---
+++ lesson6/example.py
@@ -0,0 +1,16 @@
+arr=[
+    [1,2,3,5],
+    [4,5,6,4],
+    [7,8,9,4],
+    [4,5,6,4],
+]
+n = len(arr)
+sumi=0
+for i in range(n):
+    m = len(arr[i])
+    for j in range(m):
+        if i+j+1<n:
+            sumi=sumi+arr[i][j]
+print(sumi)
+
+        
