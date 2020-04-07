04108a2ec9673075f923c15f6c05db4a578bb921
Your Name <tleugazy98.com>
2020-04-07 19:21:21 +0600

    asdsdads

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,14 +1,16 @@
 arr=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [1,2,3,5],
+    [4,5,6,4],
+    [7,8,9,4],
+    [4,5,6,4],
 ]
 n = len(arr)
+equal=n-1
 sumi=0
 for i in range(n):
     m = len(arr[i])
     for j in range(m):
-        if i==j:
+        if i+j==equal:
             sumi=sumi+arr[i][j]
 print(sumi)
 
