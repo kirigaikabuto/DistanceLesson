e90c5c7a8ad15419f6799bb762f5a19ad32e6ffc
Your Name <tleugazy98.com>
2020-04-09 20:43:48 +0600

    asdsdads

---
+++ lesson7/5.py
@@ -0,0 +1,17 @@
+import random
+n,m = input().split()
+n = int(n)
+m = int(m)
+arr=[]
+for i in range(n):
+    temp=[]
+    for j in range(m):
+        temp.append(0)
+    arr.append(temp)
+
+for i in range(n):
+    index=random.randint(0,m)
+    arr[i][index]=1
+for i in range(n):
+    print(arr[i])
+        
