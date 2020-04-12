463792febd99d6129432ed4926422a2379eb328a
Your Name <tleugazy98.com>
2020-04-12 19:22:17 +0600

    asdsdads

--- lesson8/main.py
+++ lesson8/main.py
@@ -1,11 +1,21 @@
 #Yerassyl +7086394516
 #Anel +707043435435
 #China 1232132121314
-user1=["Yerassyl","+7086394516","makataeva117","tleugazy98@gmail.com"]
-user2=["Anel","+707043435435","makataeva117","tleugazy98@gmail.com"]
-user3=["China","1232132121314","makataeva117","tleugazy98@gmail.com"]
-contacts=[user1,user2,user3]
+
+user1 = {
+    "name":"Yerassyl",
+    "phone":"7086394516",
+    "address":"makataeva117",
+    "email":"tleugazy98@gmail.com"
+}
+user2={
+     "name":"Anel",
+    "phone":"707043435435",
+    "address":"makataeva117",
+    "email":"tleugazy98@gmail.com"
+}
+contacts=[user1,user2]
 start=input("Name:")
 for i in contacts:
-    if i[0]==start:
-        print(i[1],i[2],i[3])
+    if i["name"]==start:
+        print(i["phone"],i["address"],i["email"])
