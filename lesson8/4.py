cb12c156895b8a2d0b75eec9d23f14b9a8aa48eb
Your Name <tleugazy98.com>
2020-04-12 20:08:30 +0600

    asdsdads

--- lesson8/4.py
+++ lesson8/4.py
@@ -9,3 +9,21 @@ student2={
     "marks":[3,4,2,3,4],
 }
 students=[student1,student2]
+#code
+students=[]
+students.append(student1)
+students.append(student2)
+for i in students:
+    marks=i['marks']
+    n = len(marks)
+    sumi=0
+    maxi = marks[0]
+    for j in range(n):
+        sumi = sumi + marks[j]
+        if marks[j]>maxi:
+            maxi=marks[j]
+    i['mark']=sumi/n
+    i['max']=maxi
+
+for i in students:
+    print(i)
