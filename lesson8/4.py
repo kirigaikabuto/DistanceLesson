6ddb8a4415ebe98974a91896da362a60987750ba
Your Name <tleugazy98.com>
2020-04-12 19:29:35 +0600

    asdsdads

---
+++ lesson8/4.py
@@ -0,0 +1,11 @@
+student1={
+    "name":"era",
+    "mark":4.5,
+    "marks":[5,5,5,3,4],
+}
+student2={
+    "name":"china",
+    "mark":5,
+    "marks":[3,4,2,3,4],
+}
+students=[student1,student2]
