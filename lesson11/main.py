38031d31ce5ed525da4906b01706520ecc1d2a96
Your Name <tleugazy98.com>
2020-04-18 19:32:35 +0600

    asdsdads

--- lesson11/main.py
+++ lesson11/main.py
@@ -1,13 +1,11 @@
 import csv
 import json
-csv_file = "table.csv"
-json_file = "table.json"
-arr=[]
-with open(csv_file) as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for i in csvReader:
-        arr.append(i)
-    
-with open(json_file,"w") as jsonFile:
-    json_arr= json.dumps(arr,indent=4)
-    jsonFile.write(json_arr)
+def save_csv_to_json(csv_file,json_file):
+    arr=[]
+    with open(csv_file) as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for i in csvReader:
+            arr.append(i)
+    with open(json_file,"w") as jsonFile:
+        json_arr= json.dumps(arr,indent=4)
+        jsonFile.write(json_arr)
