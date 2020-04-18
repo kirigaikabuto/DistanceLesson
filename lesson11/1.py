b4d2f0656e305d4797fcd72d135cd5e35dc4180d
Your Name <tleugazy98.com>
2020-04-18 20:10:28 +0600

    asdsdads

--- lesson11/1.py
+++ lesson11/1.py
@@ -1,5 +1,12 @@
 from main import save_csv_to_json,get_data_from_json
 
-save_csv_to_json("table.csv","table.json")
+#save_csv_to_json("table.csv","table.json")
 data = get_data_from_json("table.json")
-print(data[0])
+# employee_id=int(input("Employee_id:"))
+# first_name = input("FirstName")
+d={}
+d["EMPLOYEE_ID"]=int(input("Employee_id:"))
+d["FIRST_NAME"]=input("FirstName:")
+#...
+1)в существующий массив из словарей добавить текущий словарб
+2)сохранить весь массив в файл откуда был взят наш массив
