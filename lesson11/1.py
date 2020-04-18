3c8a0a735c5e6c2a00bbd88278cc70d91f84caa9
Your Name <tleugazy98.com>
2020-04-18 20:31:12 +0600

    asdsdads

--- lesson11/1.py
+++ lesson11/1.py
@@ -1,5 +1,6 @@
-from main import save_csv_to_json,get_data_from_json,add_object
-d={}
-d["EMPLOYEE_ID"]=int(input("Employee_id:"))
-d["FIRST_NAME"]=input("FirstName:")
-add_object("table.json",d)
+from main import add_object
+def add_employee():
+    d={}
+    d["EMPLOYEE_ID"]=int(input("Employee_id:"))
+    d["FIRST_NAME"]=input("FirstName:")
+    add_object("table.json",d)
