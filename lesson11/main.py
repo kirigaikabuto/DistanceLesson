7e5cc1c91455e7dee588cf38ae34eaafb5241b4a
Your Name <tleugazy98.com>
2020-04-18 19:49:54 +0600

    asdsdads

--- lesson11/main.py
+++ lesson11/main.py
@@ -2,18 +2,19 @@ import csv
 import json
 def save_csv_to_json(csv_file,json_file):
     arr=[]
+    #read from csv and convert to [{},{},{}]
     with open(csv_file) as csvFile:
         csvReader = csv.DictReader(csvFile)
         for i in csvReader:
             arr.append(i)
-
+    #transform from str to int
     for i in range(len(arr)):
         d = arr[i]
         d["EMPLOYEE_ID"]=int(d["EMPLOYEE_ID"])
         if d["DEPARTMENT_ID"] !=" - ":
             d["DEPARTMENT_ID"]=int(d["DEPARTMENT_ID"])
         d["SALARY"]=int(d['SALARY'])
-        
+    #write data to json file  
     with open(json_file,"w") as jsonFile:
         json_arr= json.dumps(arr,indent=4)
         jsonFile.write(json_arr)
